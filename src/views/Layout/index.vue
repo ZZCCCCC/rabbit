cc02b02b363494d60c8e0a7efc9c8e6de57fab3a
zzc <3210696396@qq.com>
2023-06-18 19:27:27 +0800

    静态结构搭建

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,8 +1,13 @@
-<script setup></script>
+<script setup>
+import LayoutNav from '@/views/Layout/components/LayoutNav.vue'
+import LayoutHeader from '@/views/Layout/components/LayoutHeader.vue'
+import LayoutFooter from '@/views/Layout/components/LayoutFooter.vue'
+</script>
+
 <template>
-    <h1>layout</h1>
-<router-view></router-view>
-
+  <LayoutNav />
+  <LayoutHeader />
+  <RouterView />
+  <LayoutFooter />
 </template>
-
 <style></style>
