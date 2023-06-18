cf4e2970e132e41e97b24ea68d983248f3bf3680
zzc <3210696396@qq.com>
2023-06-18 17:45:22 +0800

    elementPlue自动按需引入

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,19 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-// import HelloWorld from './components/HelloWorld.vue'c
-import HelloWorld from '@/components/HelloWorld.vue'
+
 </script>
 
-<template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+ <template>
+  <el-row class="mb-4">
+    <el-button>Default</el-button>
+    <el-button type="primary">Primary</el-button>
+    <el-button type="success">Success</el-button>
+    <el-button type="info">Info</el-button>
+    <el-button type="warning">Warning</el-button>
+    <el-button type="danger">Danger</el-button>
+  </el-row>
 </template>
 
-<style scoped>
+<style  scoped>
 header {
   line-height: 1.5;
   max-height: 100vh;
