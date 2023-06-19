ab6ee277d3c932ed527e7a2e9d4bcaa205c40c33
zzc <3210696396@qq.com>
2023-06-19 12:43:50 +0800

    success

--- src/stores/category.js
+++ src/stores/category.js
@@ -3,6 +3,9 @@ import { defineStore } from 'pinia'
 import { getCategoryAPI } from '@/apis/layout'
 export const useCategoryStore = defineStore('category', () => {
 
+    // 优化组件layoutFixed和layoutHeader队友categoryList的请求
+
+
     // 初始化列表项 state
     const categoryList = ref([])
     // action
