9571cc356b6188570bfefece58b95ebcdc16351f
zzc <3210696396@qq.com>
2023-06-19 19:57:21 +0800

    激活状态和分类列表

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -1,4 +1,5 @@
 <script setup>
+import GoodsItem from "./../Home/components/GoodsItem.vue";
 import { getCategoryAPI } from "@/apis/category";
 import { getBannerAPI } from "@/apis/home";
 import { ref, onMounted, watch } from "vue";
@@ -61,6 +62,31 @@ onMounted(() => {
             <img :src="item.imgUrl" alt="" />
           </el-carousel-item>
         </el-carousel>
+      </div>
+
+      <!-- 分类 -->
+      <div class="sub-list">
+        <h3>全部分类</h3>
+        <ul>
+          <li v-for="i in categoryData.children" :key="i.id">
+            <RouterLink to="/">
+              <img :src="i.picture" />
+              <p>{{ i.name }}</p>
+            </RouterLink>
+          </li>
+        </ul>
+      </div>
+      <div
+        class="ref-goods"
+        v-for="item in categoryData.children"
+        :key="item.id"
+      >
+        <div class="head">
+          <h3>- {{ item.name }}-</h3>
+        </div>
+        <div class="body">
+          <GoodsItem v-for="good in item.goods" :good="good" :key="good.id" />
+        </div>
       </div>
     </div>
   </div>
