396000352baa48f575fe55524a5639eb119f9120
zzc <3210696396@qq.com>
2023-06-21 09:22:06 +0800

    二级分类

--- src/router/index.js
+++ src/router/index.js
@@ -3,13 +3,15 @@ import Layout from '@/views/Layout/index.vue'
 import Login from '@/views/Login/index.vue'
 import Home from '@/views/Home/index.vue'
 import Category from '@/views/Category/index.vue'
+import SubCategory from '@/views/SubCategory/index.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/', component: Layout, children: [
         { path: '', component: Home },
-        { path: 'category/:id', component: Category }
+        { path: 'category/:id', component: Category },
+        { path: 'category/sub/:id', component: SubCategory }
       ]
     },
     { path: '/login', component: Login }
